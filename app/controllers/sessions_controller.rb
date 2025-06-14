81da817cd950d4855c4885f3a3b68836198d653b
Bachard Jamil <jamilbachardyahaya@gmail.com>
2025-06-14 21:19:52 +0100

    Update flash messages in SessionsController and clean up new session view
    - Enhanced user feedback by updating flash messages in SessionsController to French.
    - Removed redundant flash message display logic from new session view for cleaner UI.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,14 +9,14 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to after_authentication_url, notice: "Connexion réussie. Bienvenue #{user.username} !"
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      redirect_to new_session_path, alert: "Email ou mot de passe incorrect."
     end
   end
 
   def destroy
     terminate_session
-    redirect_to new_session_path
+    redirect_to new_session_path, notice: "Vous avez été déconnecté avec succès."
   end
 end
