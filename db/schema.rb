5f27ae12853ceafb88b2179e55454488a79edcd4
Bachard Jamil <jamilbachardyahaya@gmail.com>
2025-06-14 11:08:10 +0100

    Add user authentication and password management features
    - Implemented user authentication with secure password handling using bcrypt.
    - Created SessionsController for user login/logout functionality.
    - Developed PasswordsController for password reset functionality, including email notifications.
    - Added views for user sign-in and password management.
    - Introduced Current model to manage session state.
    - Updated routes to support session and password resources.
    - Seeded database with initial user data for testing.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_12_214739) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_14_094325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,6 +34,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_12_214739) do
     t.index ["slug"], name: "index_lists_on_slug", unique: true
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -45,5 +54,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_12_214739) do
     t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "username", null: false
+    t.string "phone_number", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
   add_foreign_key "tasks", "lists"
 end
