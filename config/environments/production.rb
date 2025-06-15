236191daf5f117238a46c98c4ac34ee2c265dad4
Bachard Jamil <jamilbachardyahaya@gmail.com>
2025-06-15 01:29:18 +0100

    Refactor production database configuration in database.yml to restore and organize settings for primary, cache, queue, and cable connections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,4 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  config.active_record.database_selector = nil
-  config.active_record.database_resolver = nil
-  config.active_record.database_resolver_context = nil
 end
-
